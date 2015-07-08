16f618f5db5170004fbfd1111b6d1847760d982d
evgeni.veselov <evgeni.veselov@gmail.com>
2015-07-08 12:01:52 +0300

    no message

--- BomgarTest/TestCtl.cs
+++ BomgarTest/TestCtl.cs
@@ -2,6 +2,7 @@
 using MonoTouch.Dialog;
 using UIKit;
 using BomgarSdkBinding;
+using Foundation;
 
 namespace BomgarTest
 {
@@ -9,18 +10,19 @@ namespace BomgarTest
     {
         private BomgarSession _session;
         private MyBomgarDelegate _delegate;
-
+        private EntryElement _sessionKey;
         public TestCtl() : base(UITableViewStyle.Grouped, null)
         {
-            _session = new BomgarSession("Level10", "supportlevel10.bomgar.com", 443);
-            _delegate = new MyBomgarDelegate();
+            _session = new BomgarSession("level_10", "supportlevel10.bomgar.com", 443);
+            _delegate = new MyBomgarDelegate(_session);
             _session.Delegate = _delegate;
 
-
+            _sessionKey = new EntryElement("SessionKey","SessionKey","");
             Root = new RootElement("")
                 { 
                     new Section("")
                     {
+                        _sessionKey,
                         new StringElement("StartWithSessionKey", ()=> StartWithSessionKey()),
                         new StringElement("EndSession", ()=> EndSession()),
 
@@ -35,12 +37,18 @@ namespace BomgarTest
 
         void StartWithSessionKey()
         {
-            _session.StartWithSessionKey("7408330", "test", (int)SessionFeature.ScreenSharing);
+            _session.StartWithSessionKey(_sessionKey.Value, "test", (int)SessionFeature.ScreenSharing);
         }
     }
 
     public class MyBomgarDelegate : BomgarSessionDelegate
     {
+        BomgarSession _session;
+        public MyBomgarDelegate(BomgarSession session)
+        {
+            _session = session;
+        }
+
         #region implemented abstract members of BomgarSessionDelegate
         public override void SessionDidConnect()
         {
@@ -57,6 +65,27 @@ namespace BomgarTest
         public override void SessionStartDidFailWithError(Foundation.NSError error)
         {
             Console.WriteLine("SessionStartDidFailWithError {0}", error.Description);
+        }
+
+        public override void ShowPromptWithId(int promptId, string message, double timeout, NSObject[] buttonTitles)
+        {
+            for (int i = 0; i < buttonTitles.Length; i++)
+            {
+                Console.WriteLine("ShowPromptWithId buttonTitles {0}", buttonTitles[i]);
+            }
+            Console.WriteLine("ShowPromptWithId Message {0}", message);
+            Console.WriteLine("ShowPromptWithId timeout {0}", timeout);
+            Console.WriteLine("ShowPromptWithId promptId {0}", promptId);
+            var alert = new UIAlertView("Message",message, null, buttonTitles[0].ToString(), buttonTitles[1].ToString());
+            alert.Tag = promptId;
+            alert.Clicked += Alert_Clicked;
+            alert.Show();
+        }
+
+        void Alert_Clicked (object sender, UIButtonEventArgs e)
+        {
+            var promtId = ((UIAlertView)sender).Tag;
+            _session.SendResponse((int)e.ButtonIndex, (int)promtId);
         }
         #endregion
         
--- ClassicBomgarSdkBinding/BomgarSession.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("BomgarSession.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64, SmartLink = true, LinkerFlags = "-lstdc++ -ObjC", ForceLoad = true, Frameworks = "CFNetwork Accelerate", IsCxx = true)]
